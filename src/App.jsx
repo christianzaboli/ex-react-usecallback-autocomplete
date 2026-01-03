14e580ebcea3c0e445f03e0b44ecf81b18bc228d
christianzaboli <zabolichristian@gmail.com>
2026-01-03 17:18:38 +0100

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>Ricerca intelligente</h1>
+        <input type="text"
+        value={}
+        onChange={}
+        placeholder="Cerca..." />
+      </div>
+    </>
+  );
 }
 
 export default App;
