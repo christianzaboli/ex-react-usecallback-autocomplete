f2b4adbe079b1a3fb7cb0c5b7312503e4e8c50b1
osaze <osazezaboli@gmail.com>
2026-01-03 20:13:27 +0100

    milestone 1 campo di ricerca dinamico con suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import "./App.css";
-
+import { useState, useEffect, useMemo } from "react";
 function App() {
+  // input ricerca
+  const [search, setSearch] = useState("");
+  const [suggest, setSuggest] = useState([]);
+
+  useEffect(() => {
+    if (search === "") return setSuggest([]);
+    fetch(`http://localhost:3333/products?search=${search}`)
+      .then((res) => res.json())
+      .then((data) => setSuggest(data))
+      .catch((err) => console.error(err));
+  }, [search]);
   return (
     <>
       <div>
         <h1>Ricerca intelligente</h1>
-        <input type="text"
-        value={}
-        onChange={}
-        placeholder="Cerca..." />
+        <div className="suggest-container">
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Cerca..."
+            list="products-suggestions"
+          />
+          <datalist id="products-suggestions">
+            {suggest?.map((p) => {
+              return (
+                <option value={p.name}>
+                  {p.brand} - {p.price}€
+                </option>
+              );
+            })}
+          </datalist>
+        </div>
       </div>
     </>
   );
